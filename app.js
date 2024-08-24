61d8322168c9c77478a686d8f8f4e8b5a3170682
jainiljain31 <jainiljain31@gmail.com>
2024-08-24 12:43:06 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+require('dotenv').config()
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const cokkieParser = require("cookie-parser");
+const userRoute = require("./routes/user");
+const blogRoute = require("./routes/blog");
+const Blog = require("./models/blog");
+const cookieParser = require("cookie-parser");
+const { checkForAuthenticationCookie } = require("./middlewares/authorization");
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("Failed to connect to MongoDB: ", error);
+  });
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.static(path.resolve("./public")));
+app.use(checkForAuthenticationCookie("token"));
+
+app.use("/user", userRoute);
+app.use("/blog", blogRoute);
+
+app.get("/", async (req, res) => {
+  const allBlogs = await Blog.find({});
+  res.render("home", {
+    user: req.user,
+    blogs: allBlogs,
+  });
+});
+app.listen(PORT, () => {
+  console.log(`Server listen at port:${PORT}`);
+});
